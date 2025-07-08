6e093ea6cf46f7427bafcf0801bae30b9cb33232
Rohitmeena8923 <157653731+Rohitmeena8923@users.noreply.github.com>
2025-07-08 22:20:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ load_dotenv()
 
 API_ID = int(os.getenv("API_ID", "27775431"))
 API_HASH = os.getenv("API_HASH", "b70bb1d45a1d05236671d4cc615e40f9")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7759314040:AAHKphPR3he8JVlvcQ6QJ-L2aCWfPPbTbSU")
 CREDIT = os.getenv("CREDIT", "Lucifer ❤️")
