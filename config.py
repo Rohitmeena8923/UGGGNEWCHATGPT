3a28d455954b2fc272fefad1ca920ffbef2fa6dc
Rohitmeena8923 <157653731+Rohitmeena8923@users.noreply.github.com>
2025-07-08 22:28:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-API_ID = int(os.getenv("API_ID", "27775431"))
-API_HASH = os.getenv("API_HASH", "b70bb1d45a1d05236671d4cc615e40f9")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7759314040:AAHKphPR3he8JVlvcQ6QJ-L2aCWfPPbTbSU")
-CREDIT = os.getenv("CREDIT", "Lucifer ❤️")
+API_ID = int(os.environ.get("API_ID", "27775431"))          # अपनी API_ID यहाँ डालो
+API_HASH = os.environ.get("API_HASH", "b70bb1d45a1d05236671d4cc615e40f9")    # API_HASH यहाँ डालो
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7759314040:AAHKphPR3he8JVlvcQ6QJ-L2aCWfPPbTbSU") # बोट टोकन यहाँ डालो
+CREDIT = "Lucifer ❤️"  # जो भी आप चाहते हो Extracted By में दिखाने के लिए
